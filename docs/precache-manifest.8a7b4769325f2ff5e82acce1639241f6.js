103505b88be14d9e5dafdc88da0554b9cefbf9c6
Autioch <autioch@gmail.com>
2018-11-16 23:32:40 +0100

    Docs updated.

---
+++ docs/precache-manifest.8a7b4769325f2ff5e82acce1639241f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae89a2905e9b0e4ebd9a",
+    "url": "/flashcards-app/static/js/runtime~main.ae89a290.js"
+  },
+  {
+    "revision": "3b6b88c33a012e9b9908",
+    "url": "/flashcards-app/static/js/main.3b6b88c3.chunk.js"
+  },
+  {
+    "revision": "e6b8e08c1bcc32ee1b13",
+    "url": "/flashcards-app/static/js/1.e6b8e08c.chunk.js"
+  },
+  {
+    "revision": "3b6b88c33a012e9b9908",
+    "url": "/flashcards-app/static/css/main.7101cd50.chunk.css"
+  },
+  {
+    "revision": "e6b8e08c1bcc32ee1b13",
+    "url": "/flashcards-app/static/css/1.68bcaa8e.chunk.css"
+  },
+  {
+    "revision": "79f814765388a59ae7b478d5ab580e76",
+    "url": "/flashcards-app/index.html"
+  }
+];
